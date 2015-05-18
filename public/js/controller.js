b72043046431a746bc399d150fe73385fd3d57d3
Falk Mueller <falk@leemia.de>
2015-05-18 22:21:52 +0200

    Api angebunden, design bearbeitet

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,31 +1,20 @@
-app.controller('StartController', function($scope){
+app.controller('StartController', function($scope, $http){
 
     var data = {query: "SELECT ?subject ?predicate ?object WHERE {?subject ?predicate ?object} LIMIT 25"};
               
-               $.ajax({
-                url: "http://localhost:8080/fuseki/ds/query",
-                method: "POST",
-                data: data,
-                success: function(res){console.log(res);},
-                error: function(res){console.log(res);}
+              //$scope.items = [1,2,3];
+              
+              GetJobs(function(res){
+                  $scope.$apply(function () {
+                        $scope.items = res;
+                     });
               });
+              
 });
 
 app.controller('AddController', function($scope){
     $scope.AddEntity = function(Entity){
-         console.log(Entity);
          
-          data = {update: "PREFIX dc: <http://leemia.de/> INSERT DATA { dc:" + Entity.kennung + "    dc:title    \"" + Entity.testtext + "\" ; dc:creator  \"" + Entity.testtext + "\" .}"};
-              
-               $.ajax({
-                url: "http://" + Entity.benutzername + ":" + Entity.passwort + "@localhost:8080/fuseki/ds/update",
-                method: "POST",
-                data: data,
-                xhrFields: {
-                    withCredentials: true
-                },
-                success: function(res){console.log(res);},
-                error: function(res){console.log(res);}
-              });
-     }
+         AddJob(Entity.benutzername, Entity.passwort, Entity);
+     };
 });
--- public/js/library.js
+++ public/js/library.js
@@ -1,45 +1,30 @@
 /*js Library*/
 
-function test(){
-    $.ajax({
-                url: "http://localhost:8080/fuseki/ds/get",
-                method: "GET",
-                success: function(res){console.log(res);},
-                error: function(res){console.log(res);}
-              });
-              
-              //query=%0A%0ASELECT+%3Fsubject+%3Fpredicate+%3Fobject%0AWHERE+%7B%0A++%3Fsubject+%3Fpredicate+%3Fobject%0A%7D%0ALIMIT+25
-              var data = {query: "SELECT ?subject ?predicate ?object WHERE {?subject ?predicate ?object} LIMIT 25"};
+function AddJob(Apiuser, ApiPassword, Job){
+              data = {update: "PREFIX dc: <http://leemia.de/> INSERT DATA { dc:" + Job.kennung + "    dc:title    \"" + Job.testtext + "\" ; dc:creator  \"" + Job.testtext + "\" .}"};
               
                $.ajax({
-                url: "http://localhost:8080/fuseki/ds/query",
-                method: "POST",
-                data: data,
-                success: function(res){console.log(res);},
-                error: function(res){console.log(res);}
-              });
-              
-              
-//              data = {update: "PREFIX dc: <http://leemia.de/> INSERT DATA { dc:ttt    dc:title    \"Test Job\" ; dc:creator  \"A.N.Other\" .}"};
-//              
-//               $.ajax({
-//                url: "http://localhost:8080/fuseki/ds/update",
-//                method: "POST",
-//                data: data,
-//                success: function(res){console.log(res);},
-//                error: function(res){console.log(res);}
-//              });
-
-              data = {update: "PREFIX dc: <http://leemia.de/> INSERT DATA { dc:ttt    dc:title    \"Test Job\" ; dc:creator  \"A.N.Other\" .}"};
-              
-               $.ajax({
-                url: "http://test:test@localhost:8080/fuseki/ds/update",
+                url: "http://" + Apiuser + ":" + ApiPassword + "@localhost:8080/fuseki/ds/update",
                 method: "POST",
                 data: data,
                 xhrFields: {
                     withCredentials: true
                 },
                 success: function(res){console.log(res);},
+                error: function(res){console.log(res);}
+              });
+}
+
+function GetJobs(CallBackFn){
+    var data = {query: "SELECT ?subject ?predicate ?object WHERE {?subject ?predicate ?object} LIMIT 25"};
+              
+               $.ajax({
+                url: "http://localhost:8080/fuseki/ds/query",
+                method: "POST",
+                data: data,
+                success: function(res){console.log(res.results.bindings); 
+                    CallBackFn(res.results.bindings);  
+            },
                 error: function(res){console.log(res);}
               });
 }
